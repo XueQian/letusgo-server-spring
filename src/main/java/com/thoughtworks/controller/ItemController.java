fd7c55717c7ff5809591c6c47b6f24805b59546d
Xue Qian <xueqian0321@126.com>
2014-11-11 16:46:02 +0800

    implement method getItems at controller ItemController

--- src/main/java/com/thoughtworks/controller/ItemController.java
+++ src/main/java/com/thoughtworks/controller/ItemController.java
@@ -1,13 +1,27 @@
 package com.thoughtworks.controller;
 
 
+import com.thoughtworks.entity.Item;
+import com.thoughtworks.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
-@RequestMapping("/item")
+@RequestMapping("/items")
 public class ItemController {
 
+    @Autowired
+    private ItemService itemServiceImpl;
 
+    @RequestMapping(method = RequestMethod.GET)
+    public @ResponseBody
+    List<Item> getItems(){
+        return itemServiceImpl.getItems();
+    }
 
 }
