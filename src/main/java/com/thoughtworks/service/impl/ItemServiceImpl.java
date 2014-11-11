54e8349e78d7d337883e4969a9c16c27e633a0d2
Xue Qian <xueqian0321@126.com>
2014-11-11 16:28:57 +0800

    add ItemServiceImpl

---
+++ src/main/java/com/thoughtworks/service/impl/ItemServiceImpl.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.service.impl;
+
+import com.thoughtworks.entity.Item;
+import com.thoughtworks.service.ItemService;
+
+import java.util.List;
+
+public class ItemServiceImpl implements ItemService{
+    @Override
+    public List<Item> getItems() {
+        return null;
+    }
+}
