11a20a11fb273847057fe0cfb22f4d75d170f2db
Xue Qian <xueqian0321@126.com>
2014-11-11 16:07:02 +0800

    add entity Item

--- src/main/java/com/thoughtworks/entity/Item.java
+++ src/main/java/com/thoughtworks/entity/Item.java
@@ -2,5 +2,70 @@ package com.thoughtworks.entity;
 
 public class Item {
 
+    private int id;
+    private String barcode;
+    private String name;
+    private String unit;
+    private double price;
+    private int categoryId;
+
+    public Item() {
+    }
+
+    public Item(String barcode, String name, String unit, double price, int categoryId) {
+        this.barcode = barcode;
+        this.name = name;
+        this.unit = unit;
+        this.price = price;
+        this.categoryId = categoryId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
 
 }
