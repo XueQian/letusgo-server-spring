90c9076714480c34003027fcb15ccf96ccdd0325
Xue Qian <xueqian0321@126.com>
2014-11-11 16:35:07 +0800

    implement method getItems at ItemServiceImpl

--- src/main/java/com/thoughtworks/service/impl/ItemServiceImpl.java
+++ src/main/java/com/thoughtworks/service/impl/ItemServiceImpl.java
@@ -1,13 +1,25 @@
 package com.thoughtworks.service.impl;
 
+import com.thoughtworks.dao.ItemDao;
 import com.thoughtworks.entity.Item;
 import com.thoughtworks.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ItemServiceImpl implements ItemService{
+
+    public ItemServiceImpl(){
+
+    }
+
+    @Autowired
+    private ItemDao itemDaoImpl;
+
     @Override
     public List<Item> getItems() {
-        return null;
+        return itemDaoImpl.getItems();
     }
 }
