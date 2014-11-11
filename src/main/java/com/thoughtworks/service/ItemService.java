d4d82d5be8569d5c3d1f0aaa5db016ba3ce3efbe
Xue Qian <xueqian0321@126.com>
2014-11-11 16:27:47 +0800

    add method getItems at interface ItemService

--- src/main/java/com/thoughtworks/service/ItemService.java
+++ src/main/java/com/thoughtworks/service/ItemService.java
@@ -1,8 +1,13 @@
 package com.thoughtworks.service;
 
+import com.thoughtworks.entity.Item;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class ItemService {
+public interface ItemService {
+
+    public List<Item> getItems();
 
 }
