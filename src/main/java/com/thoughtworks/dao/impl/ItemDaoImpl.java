9f89bba3786d5a48182c37f9e0386701e49238ea
Xue Qian <xueqian0321@126.com>
2014-11-11 16:26:08 +0800

    implement method getItems at ItemDaoImpl

--- src/main/java/com/thoughtworks/dao/impl/ItemDaoImpl.java
+++ src/main/java/com/thoughtworks/dao/impl/ItemDaoImpl.java
@@ -1,5 +1,42 @@
 package com.thoughtworks.dao.impl;
 
-public class ItemDaoImpl {
+import com.thoughtworks.dao.ItemDao;
+import com.thoughtworks.entity.Item;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+@Repository
+public class ItemDaoImpl implements ItemDao {
+
+    public ItemDaoImpl() {
+
+    }
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Override
+    public List<Item> getItems() {
+
+        String sql = "select * from items;";
+
+        return jdbcTemplate.query(sql, new RowMapper<Item>() {
+
+            @Override
+            public Item mapRow(ResultSet rs, int rowNum) throws SQLException {
+                return new Item(rs.getInt("i_id"),
+                        rs.getString("i_barcode"),
+                        rs.getString("i_name"),
+                        rs.getString("i_unit"),
+                        rs.getDouble("i_price"),
+                        rs.getInt("i_categoryid"));
+            }
+        });
+    }
 }
