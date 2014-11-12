3591635b67c63132a7818f8688aaf6ed37fd0d38
Xue Qian <xueqian0321@126.com>
2014-11-12 21:42:25 +0800

    modify CartItemServiceImplTest

--- src/test/java/com/thoughtworks/service/CartItemServiceImplTest.java
+++ src/test/java/com/thoughtworks/service/CartItemServiceImplTest.java
@@ -27,10 +27,10 @@ public class CartItemServiceImplTest {
 
     private ItemService itemServiceImpl;
 
-    private CartItem cartItem;
-
     private Item item1 = new Item(1, "ITEM000001", "测试１", "斤", 11, 2);
     private Item item2 = new Item(2, "ITEM000002", "测试2", "斤", 11, 3);
+
+    private CartItem cartItem = new CartItem(1,item1,1);
 
     @Before
     public void before() {
