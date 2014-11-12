0115689884d2b82e342d21b3aee1633136687b96
Xue Qian <xueqian0321@126.com>
2014-11-12 09:21:12 +0800

    add method getCategories at CategoryServiceImpl

---
+++ src/main/java/com/thoughtworks/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.service.impl;
+
+import com.thoughtworks.entity.Category;
+import com.thoughtworks.service.CategoryService;
+
+import java.util.List;
+
+public class CategoryServiceImpl implements CategoryService{
+    @Override
+    public List<Category> getCategories() {
+        return null;
+    }
+}
