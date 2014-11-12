18491997de18047d7524a729a07e25d633e7e24c
Xue Qian <xueqian0321@126.com>
2014-11-12 09:15:21 +0800

    implement getCategories at CategoryDaoImpl

--- src/main/java/com/thoughtworks/dao/impl/CategoryDaoImpl.java
+++ src/main/java/com/thoughtworks/dao/impl/CategoryDaoImpl.java
@@ -1,4 +1,38 @@
 package com.thoughtworks.dao.impl;
 
-public class CategoryDaoImpl {
+import com.thoughtworks.dao.CategoryDao;
+import com.thoughtworks.entity.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public class CategoryDaoImpl implements CategoryDao{
+
+    public CategoryDaoImpl(){
+
+    }
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Override
+    public List<Category> getCategories() {
+
+        String sql = "select * from categories;";
+
+        return jdbcTemplate.query(sql, new RowMapper<Category>() {
+
+            @Override
+            public Category mapRow(ResultSet rs, int rowNum) throws SQLException {
+                return new Category(
+                        rs.getInt("c_id"),
+                        rs.getString("c_name"));
+            }
+        });
+    }
+
 }
