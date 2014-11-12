a4898a3e85c8ed817d1109e4d252c01f4679d00c
Xue Qian <xueqian0321@126.com>
2014-11-12 09:18:04 +0800

    implement method getCategories at CategoryService

--- src/main/java/com/thoughtworks/service/CategoryService.java
+++ src/main/java/com/thoughtworks/service/CategoryService.java
@@ -1,4 +1,10 @@
 package com.thoughtworks.service;
 
+import com.thoughtworks.entity.Category;
+
+import java.util.List;
+
 public interface CategoryService {
+
+    public List<Category> getCategories();
 }
