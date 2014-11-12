34e536cbdc5c5a121c3a03980a4cb2c4892e1657
liyanzi <843624084@qq.com>
2014-11-12 20:21:27 +0800

    add GET request in CartItemController

--- src/main/java/com/thoughtworks/controller/CartItemController.java
+++ src/main/java/com/thoughtworks/controller/CartItemController.java
@@ -1,11 +1,27 @@
 package com.thoughtworks.controller;
 
+import com.thoughtworks.entity.CartItem;
+import com.thoughtworks.service.CartItemService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/cartItems")
+@RequestMapping("/cartitems")
 public class CartItemController {
+
+    @Autowired
+    private CartItemService cartItemServiceImpl;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public @ResponseBody List<CartItem> getCartItems() {
+
+        return cartItemServiceImpl.getCartItems();
+    }
 
 
 }
