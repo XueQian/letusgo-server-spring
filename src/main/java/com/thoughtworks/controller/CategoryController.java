25abcd67158bc81e13e4ec4bc95005a5f035e59d
Xue Qian <xueqian0321@126.com>
2014-11-12 09:29:03 +0800

    implement method getCategories at CategoryController

--- src/main/java/com/thoughtworks/controller/CategoryController.java
+++ src/main/java/com/thoughtworks/controller/CategoryController.java
@@ -1,4 +1,26 @@
 package com.thoughtworks.controller;
 
+import com.thoughtworks.entity.Category;
+import com.thoughtworks.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/categories")
 public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryServiceImpl;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public @ResponseBody
+    List<Category> getCategories(){
+        return categoryServiceImpl.getCategories();
+    }
+
 }
