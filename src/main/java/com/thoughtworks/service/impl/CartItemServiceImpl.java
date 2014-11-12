ff9795e0d6f570b90cda0dda892529c6cc76c561
liyanzi <843624084@qq.com>
2014-11-12 20:01:24 +0800

    implement getCartItems in CartItemServiceImpl

--- src/main/java/com/thoughtworks/service/impl/CartItemServiceImpl.java
+++ src/main/java/com/thoughtworks/service/impl/CartItemServiceImpl.java
@@ -1,15 +1,31 @@
 package com.thoughtworks.service.impl;
 
+import com.thoughtworks.dao.CartItemDatabaseDao;
 import com.thoughtworks.entity.CartItem;
+import com.thoughtworks.entity.CartItemDatabase;
 import com.thoughtworks.service.CartItemService;
+import com.thoughtworks.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class CartItemServiceImpl implements CartItemService{
+    @Autowired
+    private CartItemDatabaseDao cartItemDatabaseDaoImpl;
+    private ItemService itemServiceImpl;
+
     @Override
     public List<CartItem> getCartItems() {
-        return null;
+
+        List<CartItem> cartItems = new ArrayList<CartItem>();
+        List<CartItemDatabase> cartItemsDatabase = cartItemDatabaseDaoImpl.getCartItems();
+
+        for(CartItemDatabase cartItemDatabase : cartItemsDatabase) {
+            cartItems.add(new CartItem(itemServiceImpl.getItem(cartItemDatabase.getItemId()),cartItemDatabase.getCount()));
+        }
+        return cartItems;
     }
 }
