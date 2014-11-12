1b2160e62d5eca469c9f2f4c95552553856cf1a7
Xue Qian <xueqian0321@126.com>
2014-11-12 17:22:31 +0800

    modify @RestController

--- src/main/java/com/thoughtworks/controller/CategoryController.java
+++ src/main/java/com/thoughtworks/controller/CategoryController.java
@@ -4,12 +4,11 @@ import com.thoughtworks.entity.Category;
 import com.thoughtworks.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/categories")
 public class CategoryController {
 
--- src/main/java/com/thoughtworks/controller/ItemController.java
+++ src/main/java/com/thoughtworks/controller/ItemController.java
@@ -4,12 +4,11 @@ import com.thoughtworks.entity.Item;
 import com.thoughtworks.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/items")
 public class ItemController {
 
