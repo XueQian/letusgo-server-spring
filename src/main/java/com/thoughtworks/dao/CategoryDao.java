46cf4185b7857f9ba5f431a521b02300a909a7d8
Xue Qian <xueqian0321@126.com>
2014-11-12 09:08:45 +0800

    implement method getCategories at CategoryDao

--- src/main/java/com/thoughtworks/dao/CategoryDao.java
+++ src/main/java/com/thoughtworks/dao/CategoryDao.java
@@ -1,4 +1,10 @@
 package com.thoughtworks.dao;
 
+import com.thoughtworks.entity.Category;
+
+import java.util.List;
+
 public interface CategoryDao {
+
+    public List<Category> getCategories();
 }
